2c62f77568c78a69eded2ddd7872e9d3289a0f4a
Darshana Senavirathna <darshanase.lk@gmail.com>
2018-09-05 23:06:32 +0530

    Second Commit

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ var userPref;
 var ballotBox=[];
 var voterList=["Asterix","Obilix","Kakofonix","Madukix","Martix"];
 var referenceNo=0;
-var tempUserPref;
 
 function vote(userPref){
 	referenceNo++;
+	var tempUserPref;
 	tempUserPref=voterList[userPref];
 	ballotBox.push(tempUserPref);
 	console.log("Your Preference is Captured. Your Reference No is : "+referenceNo)
